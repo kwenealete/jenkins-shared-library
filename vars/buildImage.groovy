b2bbf33a8385dc232fbd5a4991f69a7f4676b4a5
kwenealete <kwenait@yahoo.ca>
2025-01-13 19:43:09 +0100

    created the shared library functions.

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,7 +4,7 @@ def call() {
     echo "Building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t monyakwene/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push monyakwene/demo-app:jma-2.0'
     }
 }
